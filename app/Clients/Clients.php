52e82a16232fe716f798c1d71db0c8bcc3ebe9e5
MizouziE <90829439+MizouziE@users.noreply.github.com>
2022-02-23 14:19:27 +0000

    a very late initial commit...

---
+++ app/Clients/Clients.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Clients;
+
+use App\Models\Stock;
+
+interface Clients
+{
+    public function checkAvailability(Stock $stock): StockStatus;
+}
---
+++ app/Clients/Target.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Clients;
+
+use App\Models\Stock;
+
+class Target implements Clients
+{
+    public function checkAvailability(Stock $stock): StockStatus
+    {
+        //
+    }
+}
